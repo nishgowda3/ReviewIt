85e428ab8e9c237e85e80edb0c784d9913432d2c
Nishgowda3 <Nishgowda3@gmail.com>
2017-03-25 04:15:57 +0000

    added view files

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
+    before_action :find_movie, only: [:show, :edit, :update, :destroy]
 def index
+    @movies = Movie.all.order("created_at DESC") 
+end
+
+def show
 end
 
 def new
@@ -8,11 +13,21 @@ end
 
 def create
     @movie = Movie.new(movie_params)
+    
+    if @movie.save
+        redirect_to root_path
+    else
+        render 'new'
+    end
 end
 
 private
 def movie_params
     params.require(:movie).permit(:title, :description, :director)
+end
+
+def find_movie
+@movie = Movie.find(params[:id])
 end
 
 end
