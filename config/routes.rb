db0f5058a49756c2435bc4e8f4fb5357eef4a6d3
Nishgowda3 <Nishgowda3@gmail.com>
2017-04-18 00:02:48 +0000

    again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-resources :movies do
+    devise_for :users
+    resources :movies do
     resources :reviews
 end
+
 root 'movies#index'
+
 end
